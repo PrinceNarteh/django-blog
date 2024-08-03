c650cd3145f5640d10f493fe9d06444630a4cff5
Prince Narteh <prinarteh@gmail.com>
2024-08-03 15:59:57 +0000

    feat: retrieve post from database

--- posts/models.py
+++ posts/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=60)
-    content = models.TextField()
+    content = models.TextField() 
     published_date = models.DateField(auto_now=True)
